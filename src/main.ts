98827bec57991874d46b4cc42664785566c20c44
mmikulski <mauriciomikulski.dev@gmail.com>
2023-09-09 12:39:42 -0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Pipoca Agil')
+    .setDescription('API para o projeto Pipoca Agil')
+    .setVersion('1.0')
+    .addTag('pipoca-agil')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
